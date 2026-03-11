1c8b00600bdbf9093d5954396f660f17714f5a81
Saikiran Chepa <sai.406@gmail.com>
2026-03-11 15:29:51 +0530

    updating arm .sh file

--- CPP_RELATED/build_cal_accuracy_arm.sh
+++ CPP_RELATED/build_cal_accuracy_arm.sh
@@ -1,6 +1,6 @@
 rm -rf build && mkdir build && cd build
 cmake .. \
-  -DCMAKE_TOOLCHAIN_FILE=$HOME/Android/ndk/26.1.10909125/build/cmake/android.toolchain.cmake \
+  -DCMAKE_TOOLCHAIN_FILE=$HOME/NDK/android-ndk-r27d/build/cmake/android.toolchain.cmake \
   -DANDROID_ABI=arm64-v8a \
   -DANDROID_PLATFORM=android-28 \
   -DCMAKE_BUILD_TYPE=Release
